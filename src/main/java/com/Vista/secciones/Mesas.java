f29c419a7e2f815e6454fe770bb1dccf2b975f69
Lenovo <Lenovo@192.168.1.181>
2024-11-14 11:23:18 -0500

    aaaa

--- src/main/java/com/Vista/secciones/Mesas.java
+++ src/main/java/com/Vista/secciones/Mesas.java
@@ -490,7 +490,7 @@ public class Mesas extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
